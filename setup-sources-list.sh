db778dee690bd6461a4a7a26703c114ae2dec421
Laszlo Budai <laszlo.budai@outlook.com>
2019-09-20 15:48:45 +0200

    setup-sources-list.sh: install gpg2 before calling 'apt-key add'
    Signed-off-by: Laszlo Budai <laszlo.budai@outlook.com>

--- setup-sources-list.sh
+++ setup-sources-list.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+apt-get update && apt-get install -y gnupg2
 apt-key add $1.Release.key
 echo "deb http://download.opensuse.org/repositories/home:/laszlo_budai:/syslog-ng/$1 ./" | tee /etc/apt/sources.list.d/syslog-ng-obs.list
 apt-get update
